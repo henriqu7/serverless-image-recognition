dd5bba95ab3bd3f942aa6b8f5792b54a8709c166
henrique <luis.rodrigues@globant.com>
2021-03-29 19:53:49 -0300

    first

---
+++ handler.js
@@ -0,0 +1,110 @@
+"use strict";
+
+const {
+  promises: { readFile },
+} = require("fs");
+
+const { get } = require("axios");
+
+class Handler {
+  constructor({ rekoSvc, translateSvc }) {
+    this.rekoSvc = rekoSvc;
+    this.translateSvc = translateSvc;
+  }
+
+  async detectImageLabels(buffer) {
+    const result = await this.rekoSvc
+      .detectLabels({
+        Image: {
+          Bytes: buffer,
+        },
+      })
+      .promise();
+
+    const workingItems = result.Labels.filter(
+      ({ Confidence }) => Confidence > 80
+    );
+
+    const names = workingItems.map(({ Name }) => Name).join(" and ");
+    console.log(result.Labels);
+
+    return { names, workingItems };
+  }
+
+  async translateText(text) {
+    const params = {
+      SourceLanguageCode: "en",
+      TargetLanguageCode: "pt",
+      Text: text,
+    };
+    const { TranslatedText } = await this.translateSvc
+      .translateText(params)
+      .promise();
+
+    return TranslatedText.split(" e ");
+  }
+
+  formatTextResults(texts, workingItems) {
+    const finalText = [];
+    for (const indexText in texts) {
+      const nameInPortuguese = texts[indexText];
+      const confidence = workingItems[indexText].Confidence;
+      finalText.push(
+        `${confidence.toFixed(2)}% de ser do tipo ${nameInPortuguese}`
+      );
+    }
+
+    return finalText.join("\n");
+  }
+
+  async getImageBuffer(imageUrl) {
+    const response = await get(imageUrl, {
+      responseType: "arraybuffer",
+    });
+
+    const buffer = Buffer.from(response.data, "base64");
+    return buffer;
+  }
+
+  async main(event) {
+    try {
+      const { imageUrl } = event.queryStringParameters;
+      //const imgBuffer = await readFile("./images/dog.jpg");
+      console.log("Downloading Image...");
+      const imgBuffer = await this.getImageBuffer(imageUrl);
+
+      console.log("Detecting Labels...");
+
+      const { names, workingItems } = await this.detectImageLabels(imgBuffer);
+
+      const translate = await this.translateText(names);
+
+      console.log("Handling final object...");
+
+      const finalText = this.formatTextResults(translate, workingItems);
+
+      console.log("Finishing...");
+
+      return {
+        statusCode: 200,
+        body: `A imagem tem\n `.concat(finalText),
+      };
+    } catch (error) {
+      console.log("Errosr: ", error);
+      return {
+        statusCode: 500,
+        body: "Interval Server Error",
+      };
+    }
+  }
+}
+
+const aws = require("aws-sdk");
+const reko = new aws.Rekognition();
+const translator = new aws.Translate();
+const handler = new Handler({
+  rekoSvc: reko,
+  translateSvc: translator,
+});
+
+module.exports.main = handler.main.bind(handler);
